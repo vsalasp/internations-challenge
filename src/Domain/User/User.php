2f52f08f96b4c9740df6fc601c9445a0e13e9ff3
Victor Salas <vsalas@victorsalas.com>
2023-07-06 00:47:13 +0200

    update user to use value objects

--- src/Domain/User/User.php
+++ src/Domain/User/User.php
@@ -4,54 +4,48 @@ declare(strict_types=1);
 
 namespace App\Domain\User;
 
+use App\Domain\ValueObjects\Common\BoolObject;
+use App\Domain\ValueObjects\ID;
+use App\Domain\ValueObjects\Name;
 use JsonSerializable;
 
 class User implements JsonSerializable
 {
-    private ?int $id;
+    private ?ID $id;
 
-    private string $username;
+    private Name $name;
 
-    private string $firstName;
+    private BoolObject $isAdmin;
 
-    private string $lastName;
-
-    public function __construct(?int $id, string $username, string $firstName, string $lastName)
+    public function __construct(?ID $id, Name $name, BoolObject $isAdmin)
     {
         $this->id = $id;
-        $this->username = strtolower($username);
-        $this->firstName = ucfirst($firstName);
-        $this->lastName = ucfirst($lastName);
+        $this->name = $name;
+        $this->isAdmin = $isAdmin;
     }
 
-    public function getId(): ?int
+    public function getID(): ID
     {
         return $this->id;
     }
 
-    public function getUsername(): string
+    public function getName(): Name
     {
-        return $this->username;
+        return $this->name;
     }
 
-    public function getFirstName(): string
+    public function getIsAdmin(): BoolObject
     {
-        return $this->firstName;
-    }
-
-    public function getLastName(): string
-    {
-        return $this->lastName;
+        return $this->isAdmin;
     }
 
     #[\ReturnTypeWillChange]
     public function jsonSerialize(): array
     {
         return [
-            'id' => $this->id,
-            'username' => $this->username,
-            'firstName' => $this->firstName,
-            'lastName' => $this->lastName,
+            'id' => $this->id->getValue(),
+            'name' => $this->name->getValue(),
+            'isAdmin' => $this->isAdmin->isValue(),
         ];
     }
 }
